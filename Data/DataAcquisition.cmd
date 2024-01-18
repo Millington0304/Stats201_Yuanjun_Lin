0bab16fd2598b9696cc7f133186ed92ad13a2b4d
Yuanjun Lin <yuanjun.lin@dukekunshan.edu.cn>
2024-01-18 16:05:36 +0800

    Update and rename DataAcquisition.cmd to Data/DataAcquisition.cmd

---
+++ Data/DataAcquisition.cmd
@@ -0,0 +1,2 @@
+echo The data file is of excessively large size to upload, it can be accessed with an installed curl command. Copy and modify the command to run on MacOS or Linux.
+curl https://www.ncei.noaa.gov/data/nclimgrid-monthly/access/nclimgrid_tavg.nc
--- DataAcquisition.cmd
+++
@@ -1,2 +0,0 @@
-echo The data file is of large size to upload, it can be accessed with installed curl command.
-curl https://www.ncei.noaa.gov/data/nclimgrid-monthly/access/nclimgrid_tavg.nc
