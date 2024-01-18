07f9e9663040f2b82340dd99648feffa218c261d
Yuanjun Lin <yuanjun.lin@dukekunshan.edu.cn>
2024-01-18 16:27:38 +0800

    Create dataAcquisition.cmd

---
+++ dataAcquisition.cmd
@@ -0,0 +1,2 @@
+echo "Through browsing the NOAA website looking for the historical environmental meteorological data, the National Centers for Environmental Information has this clear link to the gridded related datasets"
+curl https://www.ncei.noaa.gov/data/nclimgrid-monthly/access/nclimgrid_tavg.nc
